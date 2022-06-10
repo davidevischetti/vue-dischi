c280627b81bc8146ff57df13d3a8e632aead2743
Davide <davidevischetti@gmail.com>
2022-06-10 15:25:24 +0200

    struttura base

---
+++ src/components/MyContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>salve</div>
+</template>
+
+<script>
+export default {
+  name: 'MyContent',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>ciao</div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
